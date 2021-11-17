a96f2541a523de55d74a933067cc5a1f0f5677dd
d-smith <doug.smith.mail@gmail.com>
2021-11-17 13:46:49 -0800

    imports and dead code cleanup

--- src/main/java/org/ds/flink/kinesisglue/json/application/StreamingJob.java
+++ src/main/java/org/ds/flink/kinesisglue/json/application/StreamingJob.java
@@ -2,17 +2,9 @@ package org.ds.flink.kinesisglue.json.application;
 
 import com.amazonaws.services.schemaregistry.common.GlueSchemaRegistryDataFormatDeserializer;
 import com.amazonaws.services.schemaregistry.common.configs.GlueSchemaRegistryConfiguration;
-import com.amazonaws.services.schemaregistry.deserializers.GlueSchemaRegistryDeserializer;
 import com.amazonaws.services.schemaregistry.deserializers.GlueSchemaRegistryDeserializerFactory;
-import com.amazonaws.services.schemaregistry.deserializers.GlueSchemaRegistryDeserializerImpl;
-import com.amazonaws.services.schemaregistry.flink.avro.GlueSchemaRegistryAvroDeserializationSchema;
-import com.amazonaws.services.schemaregistry.utils.AWSSchemaRegistryConstants;
-import com.amazonaws.services.schemaregistry.utils.AvroRecordType;
-import org.apache.avro.Schema;
-import org.apache.avro.generic.GenericRecord;
 import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.common.serialization.DeserializationSchema;
-import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -20,15 +12,11 @@ import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTime
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.connectors.kinesis.FlinkKinesisConsumer;
 import org.apache.flink.streaming.connectors.kinesis.config.ConsumerConfigConstants;
-import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.services.glue.model.DataFormat;
 
 import java.io.IOException;
 import java.io.Serializable;
 import java.nio.ByteBuffer;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Properties;
 
 
@@ -36,36 +24,6 @@ public class StreamingJob {
     private static final String region = "us-east-1";
     private static final String inputStreamName = "kpltest";
 
-
-    private static DataStream<GenericRecord> createSourceFromStaticConfig(StreamExecutionEnvironment env) {
-        Properties inputProperties = new Properties();
-        inputProperties.setProperty(ConsumerConfigConstants.AWS_REGION, region);
-        inputProperties.setProperty(ConsumerConfigConstants.STREAM_INITIAL_POSITION, "LATEST");
-
-        Map<String, Object> configs = new HashMap<>();
-        configs.put(AWSSchemaRegistryConstants.AWS_REGION, region);
-        configs.put(AWSSchemaRegistryConstants.SCHEMA_AUTO_REGISTRATION_SETTING, true);
-        configs.put(AWSSchemaRegistryConstants.AVRO_RECORD_TYPE, AvroRecordType.GENERIC_RECORD.getName());
-
-        final String QUOTE_SCHEMA = "{\"namespace\":\"Quote.avro\",\n"
-                + "\"type\":\"record\",\n"
-                + "\"name\":\"Quote\",\n"
-                + "\"fields\":[\n"
-                + "{\"name\":\"symbol\", \"type\":\"string\"},\n"
-                + "{\"name\":\"price\", \"type\":\"double\"}\n"
-                + "]\n"
-                + "}";
-
-        Schema schema = new Schema.Parser().parse(QUOTE_SCHEMA);
-
-        FlinkKinesisConsumer<GenericRecord> consumer = new FlinkKinesisConsumer<>(
-                inputStreamName,
-                // block 2
-                GlueSchemaRegistryAvroDeserializationSchema.forGeneric(schema, configs), inputProperties);
-
-
-        return env.addSource(consumer);
-    }
 
     public static class GSRConfigHolder {
         private static final GSRConfigHolder instance = new GSRConfigHolder();
